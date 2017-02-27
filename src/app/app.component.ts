fd72299a2c94c25ee4b2db9b2302d0aa311c2be5
hosken5 <nicholas2@126.com>
2017-02-27 18:44:50 +0800

    add global erro message process

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {HeroService} from "./service/hero.service";
+import {ModalDirective} from "ng2-bootstrap";
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,17 @@ export class AppComponent {
   pop(){
     this.title = "poped";
   }
+  global_error_message:string;
   postRequest(){
-    this.heroServie.postRequest();
+    this.heroServie.postRequest().catch(err=>{
+      this.global_error_message = err ;
+      this.childModal.show();
+    });
+  }
+
+  @ViewChild('staticModal') public childModal:ModalDirective;
+
+  public showChildModal():void {
+    this.childModal.show();
   }
 }
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,11 +1,13 @@
-import { Injectable }    from '@angular/core';
+import {Injectable}    from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
 import 'rxjs/add/operator/toPromise';
 
+
 @Injectable()
 export class HeroService {
-  
+
+
   private headers = new Headers({'Content-Type': 'application/json'});
   private heroesUrl = 'api/heroes';  // URL to web api
 
@@ -20,7 +22,8 @@ export class HeroService {
       .catch(this.handleError);
   }
   private handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
+    console.error('An error occurred', error); // how should i pop a dialog here
     return Promise.reject(error.message || error);
+
   }
 }
